ccacb9642ad6e633849b64568665e33cab5ef634
e-mm-u <uh.habiba1012@gmail.com>
2022-10-12 03:50:34 +0600

    eye icon added

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import Options from '../Options/Options';
-import './Question.css'
+import './Question.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faEye } from '@fortawesome/free-solid-svg-icons'
 
-const Question = ({ ques,n }) => {
+library.add(faEye);
+
+const Question = ({ ques, n }) => {
     const { id, question, options, correctAns } = ques;
-    let i=0;
+    let i = 0;
     return (
         <div className="question">
-            <div >
-               <p> Quiz {n} : {question.slice(3, -4)}</p>
+            <div className='ques-section'>
+                <div><p> Quiz {n} : {question.slice(3, -4)}</p></div>
+                <div><FontAwesomeIcon icon="fa-solid fa-eye"/></div>
+    
             </div>
             <div className="options">
                 {
